c92d4c1bb2ff6036cc99d890c44b2d122f381d11
chrisfaux95 <chrisfaux95@gmail.com>
2020-08-17 14:24:06 -0400

    imported in render and fs

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const inquirer = require("inquirer");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-
+const render = require("./lib/htmlRenderer");
+const fs = require("fs");
 
 const all_questions = [
     {
