48319af6c3a2afb2fa5c4edd77b1d81361fc979c
chrisfaux95 <chrisfaux95@gmail.com>
2020-08-19 14:39:41 -0400

    added comments

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
+// IMPORT OF REQUIRED PACKAGES
 const inquirer = require("inquirer");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const render = require("./lib/htmlRenderer");
 const fs = require("fs");
+//OUTPUT DIRECTORY PATH
 var dir = './output';
 
+//ARRAY OF COMMON QUESTIONS
 const all_questions = [
     {
         type: 'input',
@@ -25,24 +28,28 @@ const all_questions = [
     }
 ];
 
+//ADDING MANAGER SPECIFIC QUESTIONS
 const manager_questions = [...all_questions, {
     type: 'input',
     name: 'officeNumber',
     message: "What is their office number?"
 }]
 
+//ADDING INTERN SPECIFIC QUESTIONS
 const intern_questions = [...all_questions, {
     type: 'input',
     name: 'school',
     message: "What School did they go to/are they going to?"
 }]
 
+//ADDING ENGINEER SPECIFIC QUESTIONS
 const engineer_questions = [...all_questions, {
     type: 'input',
     name: 'github',
     message: "What is their github username?"
 }]
 
+//QUESTION FOR EMPLOYEE ROLE SELECTION
 const employee_select = [{
     type: 'list',
     name: 'role',
@@ -50,8 +57,12 @@ const employee_select = [{
     choices: ['Manager', 'Engineer', 'Intern', "Stop Adding Employees"]
 }]
 
+//CREATION OF LIST OF EMPLOYEES
 const employees = [];
 
+// BASIC FUNCTION TO VALIDIATE EMAIL ADDRESS
+// CHECKS IF IN THE FORM OF
+// [blank]@[blank].[blank]
 function validateEmail(value){
     const re_email = /\S+@\S+\.\S+/;
     if (re_email.test(value)){
@@ -60,9 +71,11 @@ function validateEmail(value){
     return "Please Enter Valid Email";
 }
 
-
+//MAIN LOOPING FUNCTION
 function addEmployees() {
+    //START THE INQUIRER PROMPT
     inquirer.prompt(employee_select).then(e => {
+        //GET EMPLOYEE ROLE AND SWITCH TO SPECIFIC FUNCTION
         switch(e.role) {
             case 'Manager':
                 addManager();
@@ -74,40 +87,52 @@ function addEmployees() {
                 addIntern();
                 break;
             default:
-                // console.log(employees);
-                // console.log(render(employees));
+                // IF CHOOSING NOT EMPLOYEE,
+                // EXIT LOOP AND MAKE HTML FILE
                 writeHTMLFile(render(employees));
                 break;
         }
     })
 }
-
+/* FUNCTIONS TO ADD EMPLOYEES */
 function addManager() {
+    // ASK MANAGER QUESTIONS
     inquirer.prompt(manager_questions).then(e => {
+        // MAKE NEW MANAGER IN EMPLOYEES
         employees.push(new Manager(e.name, e.id, e.email, e.officeNumber));
+        // RECALL FUNCTION TO ADD NEW EMPLOYEES
         addEmployees();
     });
 }
 
 function addEngineer() {
+    // ASK ENGINEER QUESTIONS
     inquirer.prompt(engineer_questions).then(e => {
+        // MAKE NEW ENGINEER IN EMPLOYEES
         employees.push(new Engineer(e.name, e.id, e.email, e.github));
+        // RECALL FUNCTION TO ADD NEW EMPLOYEES
         addEmployees();
     });
 }
 
 function addIntern() {
+    // ASK INTERN QUESTIONS
     inquirer.prompt(intern_questions).then(e => {
+        // MAKE NEW INTERN IN EMPLOYEES
         employees.push(new Intern(e.name, e.id, e.email, e.school));
+        // RECALL FUNCTION TO ADD NEW EMPLOYEES
         addEmployees();
     });
 }
 
+//FUNCTION TO WRITE GIVEN INPUT TO HTML FILE
 function writeHTMLFile(html) {
+    // NAME OF OUTPUT FILE
     const fileName = "team_page.html"
-    // console.log(html);
-    // console.log(__dirname);
+    // CHECKS IF THERE IS AN OUTPUT DIRECTORY
+    // MAKES ONE IF NOT
     if(!fs.existsSync(dir)){ fs.mkdirSync(dir); }
+    // WRITE FILE
     fs.writeFile("./output/" + fileName, html, (err) => {
         if (err) throw err;
         console.log('The page has been saved');
@@ -115,4 +140,5 @@ function writeHTMLFile(html) {
 
 }
 
+// RUN THE PROMPT SEQUENCE
 addEmployees();
